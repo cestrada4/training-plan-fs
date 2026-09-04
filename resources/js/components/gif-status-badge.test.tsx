c31762e6845f4b1f12b0384b3b4a79056c0b3d3f
cestrada4 <cestrada@fullscale.ph>
2026-09-04 16:39:00 +0800

    add more tests

--- resources/js/components/gif-status-badge.test.tsx
+++ resources/js/components/gif-status-badge.test.tsx
@@ -8,7 +8,7 @@ test.each([
     ["processing", "#FF6600", "Processing"],
     ["completed", "#17A150", "Completed"],
     ["failed", "#DC2626", "Failed"]
-])(" with status '%s' displays the correct color '%s' and label '%s'", (status, color, label) => {
+])("if with status '%s', it should display the correct color '%s' and label '%s'", (status, color, label) => {
     render(<GifStatusBadge status={status as GifStatus}/>)
 
     const statusBadge = screen.getByTestId('status-badge')
@@ -17,3 +17,22 @@ test.each([
     })
     expect(statusBadge).toHaveTextContent(label)
 } )
+
+test("should display Unknown with color #9CA3AF if GifStatus is not known", () => {
+    // @ts-expect-error for testing
+    render(<GifStatusBadge status="unknown"/>)
+
+    expect(screen.getByTestId('status-badge')).toHaveTextContent("Unknown")
+    expect(screen.getByTestId('status-badge')).toHaveStyle({
+        color: "#9CA3AF"
+    })
+})
+
+test("should call onClick if clicked on status badge", () => {
+    const mockFn = jest.fn()
+    render(<GifStatusBadge status="queued" onClick={mockFn} />)
+    
+    const statusBadgeEl = screen.getByTestId('status-badge')
+    statusBadgeEl.click();
+    expect(mockFn.mock.calls).toHaveLength(1)
+})
