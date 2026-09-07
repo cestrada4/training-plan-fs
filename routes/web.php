c64e3df3ceaaa037be52af1c198acc230fb3d497
cestrada4 <jackmord445@gmail.com>
2026-09-07 22:12:43 +0800

    add to route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PayrollExportController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
@@ -7,5 +8,8 @@ Route::inertia('/', 'welcome')->name('home');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
 });
+
+Route::get('/time-cards/export-period-totals-old', [PayrollExportController::class, 'exportPeriodTotalsOld'])->name('payrollexport.export-period-totals-old');
+Route::get('/time-cards/export-period-totals', [PayrollExportController::class, 'exportPeriodTotals'])->name('payrollexport.export-period-totals');
 
 require __DIR__.'/settings.php';
